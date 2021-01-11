9948cdb9b706dacf0295b56e5c501e0cb03aa42b
g-despot <66276597+g-despot@users.noreply.github.com>
2021-01-11 08:57:48 +0100

    Update memgraph.py

--- sng_demo/database/memgraph.py
+++ sng_demo/database/memgraph.py
@@ -9,7 +9,7 @@ MG_HOST = os.getenv('MG_HOST', '127.0.0.1')
 MG_PORT = int(os.getenv('MG_PORT', '7687'))
 MG_USERNAME = os.getenv('MG_USERNAME', '')
 MG_PASSWORD = os.getenv('MG_PASSWORD', '')
-MG_ENCRYPTED = os.getenv('MG_ENCRYPT', 'true').lower() == 'true'
+MG_ENCRYPTED = os.getenv('MG_ENCRYPT', 'false').lower() == 'true'
 
 
 class Memgraph:
